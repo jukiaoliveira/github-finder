e89386d1ea3a5f059292ddb757887b5f9455e6c3
Julia OLiveira <jurafaoliveira@gmail.com>
2026-04-03 19:52:04 -0300

    feat: add error component

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,17 +2,26 @@ import type { UserProps } from "../types/user";
 
 import Search from "../components/Search";
 import User from "../components/User";
+import Error from "../components/Error";
 
 import { useState } from "react";
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
 
     const loadUser = async (username: string) => {
+        setError(false);
+        setUser(null);
 
         const res = await fetch(`https://api.github.com/users/${username}`);
 
         const data = await res.json();
+
+        if (res.status === 404) {
+            setError(true);
+            return;
+        }
 
         const { avatar_url, login, location, followers, following } = data;
         
@@ -31,6 +40,7 @@ const Home = () => {
     <div>
         <Search loadUser={loadUser} />
         {user && <User {...user}/>}
+        {error && <Error />}
     </div>
     )
 };
